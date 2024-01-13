01020156da779f3aa19cc1942f3d01c09ef49ca6
Sergtsf <reyjjiss@gmail.com>
2024-01-13 16:05:40 +0300

    23.6.1

---
+++ Config.py
@@ -0,0 +1,6 @@
+TOKEN = '6903591483:AAG-dyKsSBOaY6GKUwyjdJwGcAixB8S2GdU'
+
+keys = {'евро': 'EUR',
+        'доллар': 'USD',
+        'рубль': 'RUB',
+}
---
+++ ExchangeMoneyBot.py
@@ -0,0 +1,41 @@
+import requests
+import telebot
+from Config import keys, TOKEN
+from Extensions import ConvertionExceprion, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = ('Чтобы начать работу введите комманду боту в следующем формате:\n <имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидеть список всех доступных валют: /values')
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionExceprion('Слишком много параметров')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except ConvertionExceprion as e:
+        bot.reply_to(message, f'Ошибка пользователя. \n{e}')
+
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду \n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
